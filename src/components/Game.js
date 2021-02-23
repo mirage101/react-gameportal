62c11471427fcef91f55385506be2c69cc57e22c
Ivan <mirage101@abv.bg>
2021-02-23 22:50:45 +0200

    upload files in git

---
+++ src/components/Game.js
@@ -0,0 +1,46 @@
+//game cards
+import React from 'react';
+//Styling and animation
+import styled from 'styled-components';
+import {motion} from 'framer-motion';
+//Redux
+import {useDispatch} from 'react-redux';
+import {loadDetail} from '../actions/detailAction';
+import {Link} from 'react-router-dom';
+import {smallImage} from '../util';
+
+const Game = ({name, released, image, id}) =>{
+    const stringPathId = id.toString();
+    const dispatch = useDispatch();
+    const loadDetailHandler = () =>{
+        document.body.style.overflow ="hidden";
+        dispatch(loadDetail(id))
+     
+    }
+
+    return(
+        <StyledGame layoutId={stringPathId} onClick={loadDetailHandler}>
+            <Link to={`/game/id`}>
+            <h3>{name}</h3>
+            <p>{released}</p>
+            <motion.img layoutId={`image ${stringPathId}`} src={smallImage(image, 640)} alt={name} />
+            </Link>
+        </StyledGame>
+    );
+}
+
+const StyledGame = styled(motion.div)`
+    min-height:30vh;
+    box-shadow: 0px 5px 20px rgba(0,0,0,0.2);
+    text-align:center;
+    border-radius: 1rem;
+    cursor:pointer; 
+    overflow:hidden;
+    img{
+        width:100%;
+        height: 40vh;
+        object-fit:cover;
+        display:block;
+    }
+`;
+export default Game;
